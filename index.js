d4c7c7af751a34ceab2f84ada45513e8cdccde8c
elkizana <53874333+Barbarosse@users.noreply.github.com>
2021-07-18 13:48:25 +0100

    Initial commit

--- index.js
+++ index.js
@@ -51,3 +51,11 @@ app.on('activate', () => {
 
 
 
+
+const path = require('path')
+const git = require('isomorphic-git')
+const http = require('isomorphic-git/http/node')
+const fs = require('fs')
+
+const dir = path.join(process.cwd(), 'test-clone')
+git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs' }).then(console.log)
