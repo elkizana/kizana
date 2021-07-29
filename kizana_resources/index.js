03fae00914ecd9b9dd350770de40e90e4701072d
elkizana <53874333+Barbarosse@users.noreply.github.com>
2021-07-29 17:33:18 +0100

    Initial commit

--- kizana_resources/index.js
+++ kizana_resources/index.js
@@ -1,4 +1,4 @@
-let kizana_version = 121442
+let kizana_version = "121442"
 
 
 const { app, BrowserWindow } = require('electron');
@@ -44,12 +44,14 @@ app.on('activate', () => {
   require('electron-reloader')(module);
 } catch (_) { }
  */
-/* 
+
 var request = require('request');
   request.get('https://elkizana.github.io/version.txt', function (error, response, body) {      // new update verfication 
       if (!error && response.statusCode == 200) {
-              if ( body !== kizana_version ) { 
-                upadte(ver_num = body )
+        console.log(Number.isInteger(kizana_version))
+              if ( toString(body) !=  toString(kizana_version)) { 
+                //upadte(ver_num = body )
+                
               }
     }
   })
@@ -74,10 +76,10 @@ const file = fs.createWriteStream("update.zip");
 
   await  console.log(ver_num)
     var data = fs.readFileSync('kizana_resources/index.js').toString().split("\n");
-    data.splice(0, 1, "let kizana_version = " + ver_num);
+    data.splice(0, 1, `let kizana_version = ${ver_num}  `);
     var text = data.join("\n");
     fs.writeFile('kizana_resources/index.js', text, function (err) {
       if (err) return console.log(err);
     })
 
-}   */
+}  
