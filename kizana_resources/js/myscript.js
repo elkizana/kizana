84a5e593337eaeea3c41977cf966e1350eafdeab
elkizana <53874333+Barbarosse@users.noreply.github.com>
2021-08-31 23:12:26 +0100

    Initial commit

--- kizana_resources/js/myscript.js
+++ kizana_resources/js/myscript.js
@@ -278,6 +278,7 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
   let next_found = ".next_found" + table_id
   let single_book_search_num = ".single_book_search_num" + table_id
   let sidebar_search_input = ".sidebar_search_input" + table_id
+  let close_search = ".close_search" + table_id
 
   $("body").append(`<div class="book_div" id="b${table_id}" > 
   <div id="book_toolbox">
@@ -294,6 +295,7 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
           <img src="./../icons/arrow_down.png" class="next_found${table_id}" id="next_found"   />  
           <img src="./../icons/arrow_up.png" class="previous_found${table_id}" id="previous_found" />
           <div id="single_book_search_num" class="single_book_search_num${table_id}"> </div>
+          <span id="close_search" class="close_search${table_id}" > x</span>
           </div>
 
           <div class="sidebar side${table_id}">   </div>    
@@ -303,7 +305,7 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
 
   knex_all.raw("SELECT COUNT(*) FROM b" + table_id_original).then(function (text_table) { table_length = Object.values(text_table[0]) })  // table length for slider length                                                                    
 
-  function content_updater(table_id_original, row_id, content_id) {
+  function content_updater(table_id_original, row_id, content_id) {                 // book text content changer
     knex_all.from("b" + table_id_original).where("id", row_id).then(function (row) {
       $(content_id).animate({ scrollTop: 0 }, 0);
       if (row[0].content.includes(delimeter)) {
@@ -328,194 +330,204 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
 
 
 
-  knex_all.from("t" + table_id_original).then(function (index) {                         /// start of book index 
-    for (x in index) {
-      if (index[x].parent == 0) {
-        $(sidebar_id).append(`<H1 id=I${index[x].id}>  ${index[x].content}</H1>`)
-      }
-      else if (index[x].parent != 0) {
-        $(sidebar_id).append(`<H2 id=I${index[x].id}>${index[x].content}</H2>`) // grab sub titles and put them under the previous level 1 tilte. 
-      }
-    }
-    $(sidebar_id).append("<H1></H1>")
+  function book_text_formation() {
 
-    
-    $(sidebar_search_input).on("keyup", function () {
-          
-      $(this).val(Oktob.replaceEnCharsAZERTY($(this).val()))
-      var filter = $(this).val() 
-        count = 0;
-      $(sidebar_id + " H1,H2").each(function () {
-        if ($(this).text().search(new RegExp(filter, "i")) < 0 && filter.length > 2 ) {
-          $(this).hide();
-        } else {
-          $(this).show();
-          count++;
+    knex_all.from("b" + table_id_original).where("id", initial_rowid).then(function (text_table) {
+      $(content_id).append(`    ${text_table[0].content.replace(line_breaking, '<br>')}      `)
+      $(page_input).val(` ${text_table[0].page || ""}  `)   
+      $(content_id).attr('id', initial_rowid);
+      $(slider_id).slider({
+        value: initial_rowid,
+        orientation: "vertical",
+        min: 1,
+        isRTL: true,
+        max: table_length,
+        slide: function (event, ui) {
+          row_id = ui.value
+          content_updater(table_id_original, row_id, content_id)
         }
-
       })
-    })
-
-  })                                                                          /// end of book index 
-
-  /// Book content 
-  knex_all.from("b" + table_id_original).where("id", initial_rowid).then(function (text_table) {
-    $(content_id).append(`    ${text_table[0].content.replace(line_breaking, '<br>')}      `)
-    $(page_input).val(` ${text_table[0].page || ""}  `)   
-    $(content_id).attr('id', initial_rowid);
-    $(slider_id).slider({
-      value: initial_rowid,
-      orientation: "vertical",
-      min: 1,
-      isRTL: true,
-      max: table_length,
-      slide: function (event, ui) {
-        row_id = ui.value
-        content_updater(table_id_original, row_id, content_id)
-      }
-    })
-
-    $(content_id).on('mousewheel', function (e) { delta = e.originalEvent.deltaY ; row_id = parseInt(this.id) ; delta > 0 ? content_updater(table_id_original, row_id + 1, content_id) : content_updater(table_id_original, row_id - 1, content_id); $(slider_id).slider("value", row_id);})
-    $(previous).on("click", function () { let a = parseInt($(content_id).attr("id")) ;  $(slider_id).slider("value", a - 1); content_updater(table_id_original, a - 1, content_id)  ; })
-    $(next).on("click", function () { let a = parseInt($(content_id).attr("id"))  ; $(slider_id).slider("value", a + 1); content_updater(table_id_original, a + 1, content_id)  ;})
-    
-
-    let found_in = [] 
-    //$( search_block ).draggable()
-    $(single_book_search_input).on("keyup", function (event) { 
-      $(this).val(  Oktob.replaceEnCharsAZERTY(     $(this).val()  )   ) ;
-       $(single_book_search_input).val().length  > 2 ? search_in_single_book() : null 
-    })   
-    
-    async function search_in_single_book() {                   
-      $( next_found ).off()
-      $(previous_found).off()
-      found_in = []
   
-      let search_input_value =   $(single_book_search_input).val().removeTashkel() 
+      $(content_id).on('mousewheel', function (e) { delta = e.originalEvent.deltaY ; row_id = parseInt(this.id) ; delta > 0 ? content_updater(table_id_original, row_id + 1, content_id) : content_updater(table_id_original, row_id - 1, content_id); $(slider_id).slider("value", row_id);})
+      $(previous).on("click", function () { let a = parseInt($(content_id).attr("id")) ;  $(slider_id).slider("value", a - 1); content_updater(table_id_original, a - 1, content_id)  ; })
+      $(next).on("click", function () { let a = parseInt($(content_id).attr("id"))  ; $(slider_id).slider("value", a + 1); content_updater(table_id_original, a + 1, content_id)  ;})
       
-      await knex_all.select("content", "id").from("b" + table_id_original).then(function (results) {
-            for (row of results) {
-              if (row.content.includes(search_input_value)) {
-                $( previous_found ).show()
-                $( next_found ).show()
-                found_in.push(row.id)
-            }
-          }
-        })
-        found_in.length > 0 ? $(single_book_search_num).html(found_in.length) : $(single_book_search_num).html("لا شيئ") && $(previous_found).hide() && $(next_found).hide() 
-      
-    let i = 0 
-    let a = 1     
+  
+      let found_in = [] 
+      //$( search_block ).draggable()
+      $(single_book_search_input).on("keyup", function (event) { 
+        $(this).val(  Oktob.replaceEnCharsAZERTY(     $(this).val()  )   ) ;
+         $(single_book_search_input).val().length  > 2 ? search_in_single_book() : null 
+      })   
 
-       $(next_found).on("click" , function () {               // next of found single book search results
-       content_updater(table_id_original,  found_in[i] , content_id)
-        $(single_book_search_num).html(  a  + " / "  + found_in.length   )
-
-              i == (found_in.length - 1) ? null :  i++
-              a == found_in.length ? null : a++ 
-
-              setTimeout(() => {
-                $( `${content_id}:contains(${search_input_value}) , ${hashia_id}:contains(${search_input_value}) `  ).html(function(_, html) {
-                  return html.split(search_input_value).join(`<span class='found_single_word'>${search_input_value}</span>`);
-              });
-              }, 500);
-            })
-
-    
-    $(previous_found).on("click" , function () {                      // previous of found single book search results
-        a ==  1 ?   null :   a--
-        i == 0 ? null : i--
-
-        content_updater(table_id_original,  found_in[i] , content_id)
-        
-        $(single_book_search_num).html( a  + " / "  +  found_in.length  )
-          setTimeout(() => {
-            $(`${content_id}:contains(${search_input_value}) , ${hashia_id}:contains(${search_input_value}) ` ).html(function(_, html) {
-              return html.split(search_input_value).join(`<span class='found_single_word'>${search_input_value}</span>`);
-          });
-          }, 500);
-    })
-
-    //$(next_found).trigger("click")
-    
-
-    }
-    
-      /**************************************************
-       * Context-Menu with Sub-Menu
-       **************************************************/
-
-       let book_inf          
-       knex_master.select("bibliography").from("biblio").where("id", table_id_original ).then(function (info) {
-         
-         if (info[0].bibliography.length > 5) {
-           book_inf = info[0].bibliography.replace(line_breaking , "\n")
-         }
-       }).catch(function () {
-         book_inf = "ليس للكتاب بطاقة"
-       })
-
-      let author_inf
-      let author_id
-      
-      knex_master.select("authors").from("book").where("book_id" , table_id_original).then(function (results) { 
-        author_id = results[0].authors
-
-        knex_master.select("inf").from("bio").where("authid" , author_id ).then(function (results) { 
-          
-          if (results[0].inf.length > 5 ) { 
-           author_inf = results[0].inf.replace(line_breaking , "\n")
-          }
-
-        }).catch(function () {
-          author_inf = "ليس للمؤلف بطاقة"
-        })
-      }) 
-
-
-       setTimeout(() => {
-
-      $.contextMenu({
-          selector: content_id, 
-          items: {
-                "close" : {"name"  : "غلق أو فتح الفهرس" ,
-                callback: function(itemKey, opt, e) {
-                  close_index()
-              }
-              } ,
-
-               
-              "search" : { "name" : "البحث في هذا الكتاب" , 
-              callback: function(itemKey, opt, e) {
-                $(search_block).slideToggle()  ; $(single_book_search_input).trigger("focus") ;
-            }
-            
-            } ,
-              "fold1": {
-                  "name": "بطاقة الكتاب", 
-                  "items": {
-                      "fold2-key": {"name": book_inf }
-                  }
-              }  , 
-
-              "fold2": {
-                "name": " بطاقة المؤلف", 
-                "items": {
-                    "fold2-key": {"name": author_inf}
-                }
-            } , 
-            
-            "bookmark" : { "name" : "جعل هذا الموضع في المفضلات" , 
-              callback: function(itemKey, opt, e) {
-                add_to_bookmarks()
-            }}
-
-
-          }
+      $(close_search).on("click" , function () {                      
+        $(search_block).slideToggle()
       })
-  }, 100);
+      
+      async function search_in_single_book() {                   
+        $( next_found ).off()
+        $(previous_found).off()
+        found_in = []
+    
+        let search_input_value =   $(single_book_search_input).val().removeTashkel() 
+        
+        await knex_all.select("content", "id").from("b" + table_id_original).then(function (results) {
+              for (row of results) {
+                if (row.content.removeTashkel().includes(search_input_value)) {
+                  $( previous_found ).show()
+                  $( next_found ).show()
+                  found_in.push(row.id)
+              }
+            }
+          })
+          found_in.length > 0 ? $(single_book_search_num).html(found_in.length) : $(single_book_search_num).html("لا شيئ") && $(previous_found).hide() && $(next_found).hide() 
+        
+      let i = 0 
+      let a = 1     
+  
+         $(next_found).on("click" , function () {               // next of found single book search results
+         content_updater(table_id_original,  found_in[i] , content_id)
+          $(single_book_search_num).html(  a  + " / "  + found_in.length)
+  
+                i == (found_in.length - 1) ? null :  i++
+                a == found_in.length ? null : a++ 
+  
+                setTimeout(() => {
+                  $(content_id).text().removeTashkel()
+                  $( `${content_id}:contains(${search_input_value}) , ${hashia_id}:contains(${search_input_value}) `).html(function(_, html) {
+                    return html.split(search_input_value).join(`<span class='found_single_word'>${search_input_value}</span>`);
+                })
+                }, 500);
+              })
+       
+      $(previous_found).on("click" , function () {                      // previous of found single book search results
+          a ==  1 ?   null :   a--
+          i == 0 ? null : i--
+  
+          content_updater(table_id_original,  found_in[i] , content_id)
+          
+          $(single_book_search_num).html( a  + " / "  +  found_in.length  )
+            setTimeout(() => {
+              $(`${content_id}:contains(${search_input_value}) , ${hashia_id}:contains(${search_input_value}) ` ).html(function(_, html) {
+                return html.split(search_input_value).join(`<span class='found_single_word'>${search_input_value}</span>`);
+            });
+            }, 500);
+      })
 
+
+  
+  
+      }
+      
+         let book_inf          
+         knex_master.select("bibliography").from("biblio").where("id", table_id_original ).then(function (info) {
+           if (info[0].bibliography.length > 5) {
+             book_inf = info[0].bibliography.replace(line_breaking , "\n")
+           }
+         }).catch(function () {
+           book_inf = "ليس للكتاب بطاقة"
+         })
+  
+        let author_inf
+        let author_id
+        
+        knex_master.select("authors").from("book").where("book_id" , table_id_original).then(function (results) { 
+          author_id = results[0].authors
+  
+          knex_master.select("inf").from("bio").where("authid" , author_id ).then(function (results) { 
+            
+            if (results[0].inf.length > 5 ) { 
+             author_inf = results[0].inf.replace(line_breaking , "\n")
+            }
+  
+          }).catch(function () {
+            author_inf = "ليس للمؤلف بطاقة"
+          })
+        }) 
+  
+  
+         setTimeout(() => {
+  
+        $.contextMenu({
+            selector: content_id, 
+            items: {
+                  "close" : {"name"  : "غلق أو فتح الفهرس" ,
+                  callback: function(itemKey, opt, e) {
+                    close_index()
+                }
+                } ,
+  
+                 
+                "search" : { "name" : "البحث في هذا الكتاب" , 
+                callback: function(itemKey, opt, e) {
+                  $(search_block).slideToggle()  ; $(single_book_search_input).trigger("focus") ;
+              }
+              
+              } ,
+                "fold1": {
+                    "name": "بطاقة الكتاب", 
+                    "items": {
+                        "fold2-key": {"name": book_inf }
+                    }
+                }  , 
+  
+                "fold2": {
+                  "name": " بطاقة المؤلف", 
+                  "items": {
+                      "fold2-key": {"name": author_inf}
+                  }
+              } , 
+              
+              "bookmark" : { "name" : "جعل هذا الموضع في المفضلات" , 
+                callback: function(itemKey, opt, e) {
+                  add_to_bookmarks()
+              }}
+  
+  
+            }
+        })
+    }, 100);
+  
+    })                                                               // end text table  selection
+
+
+
+    
+  }
+
+  
+
+
+  async  function index_formation_and_behaviors() {
+
+      await knex_all.from("t" + table_id_original).then(function (index) {                         /// start of book index 
+        for (x in index) {
+          if (index[x].parent == 0) {
+            $(sidebar_id).append(`<H1 id=I${index[x].id}>  ${index[x].content}</H1>`)
+          }
+          else if (index[x].parent != 0) {
+            $(sidebar_id).append(`<H2 id=I${index[x].id}>${index[x].content}</H2>`) // grab sub titles and put them under the previous level 1 tilte. 
+          }
+        }
+        $(sidebar_id).append("<H1></H1>")
+        $(sidebar_search_input).on("keyup", function () {
+              
+          $(this).val(Oktob.replaceEnCharsAZERTY($(this).val()))
+          var filter = $(this).val() 
+            count = 0;
+          $(sidebar_id + " H1,H2").each(function () {
+            if ($(this).text().search(new RegExp(filter, "i")) < 0 && filter.length > 2 ) {
+              $(this).hide();
+            } else {
+              $(this).show();
+              count++;
+            }
+    
+          })
+        })
+        
+        $(sidebar_search_input).css("width" , $(sidebar_id).width() ) // give width to sidebar_search_input
+      })                                                                          /// end of book index 
 
     $('body').on('DOMSubtreeModified', content_id, function () {                               // menu tracker 
       if ($(content_id + " [data-type='title']").length != 0 && $(sidebar_id).width() != 0) {
@@ -527,8 +539,10 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
       }
       else { null }
     })
-
+  
+  
     $(sidebar_id + " H2").toggle()                                  // Initiate the index in collappsed state 
+
     $(sidebar_id + " H1 , H2").on("click", function () {                        // On click  toggle the title sub-titles and scroll to correspondent text in main book window                     
       this.tagName == "H1" ? $(this).nextUntil("H1").toggle('slow') : null;
       row_id = $(this).attr("id").slice(1)
@@ -538,22 +552,42 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
       }).catch(function () {
         null
       })
-    })
-    $(sidebar_search_input).css("width" , $(sidebar_id).width() ) // give width to sidebar_search_input
-  })                                                               // end text table  selection
 
-  $(".chrome-tab-close").on("click", function () {              /*          closing tabs            */
+    })
+  
+
+  }
+
+
+  book_text_formation()
+  index_formation_and_behaviors()
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  $(".chrome-tab-close").on("click", function () {              //          closing tabs            
     $("#" + this.id.substring(1)).remove()
     $("#b" + this.id.substring(1)).remove()
   })
 
-  $(".chrome-tab").on("click", function () {                                    /*          navigating between tabs            */
+  $(".chrome-tab").on("click", function () {                                    //          navigating between tabs            
     $(".book_div").hide()
     $("#b" + this.id).show()
   })
 
   let width
-   function close_index() {
+   function close_index() {                           // close/open book index 
 
     if ($(sidebar_id).width() != 0) {
       width = $(sidebar_id).width()
@@ -571,13 +605,16 @@ function add_book(table_id, initial_rowid = "1") {                    // add boo
     }
   }
 
-function add_to_bookmarks() {
+function add_to_bookmarks() {                           // add_to_bookmarks
 let rawdata = fs.readFileSync('kizana_resources/js/bookmark.json');
 let bookmarks_list = JSON.parse(rawdata);
 bookmarks_list.push({"name" : book_title , "page" : $(page_input ).val() || "1" , "page_id" : $(content_id).attr("id") , "part" : $(part_input).val()  ,   "id" : table_id_original} )
 bookmarks_list = JSON.stringify(bookmarks_list)
 fs.writeFileSync('kizana_resources/js/bookmark.json', bookmarks_list ) 
 }
+
+
+
 }                  /// end add_book
 
 function add_book_and_tab(tab_title, id, initial_rowid) {
